92da7222241db7e3e4d7e34af4bfbd5e08e89e4d
owalker10 <36974651+owalker10@users.noreply.github.com>
2020-07-14 21:45:47 -0500

    successfully generated data json

--- dataPreprocessing/generateFeatureData.py
+++ dataPreprocessing/generateFeatureData.py
@@ -2,66 +2,104 @@ import pandas as pd
 import re
 import spotipyUtils as sp
 import numpy as np
+import os
+import json
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
 
 
 # ['1940','1950','1960','1970','1980','1990','2000','2010']
-decades = ['1940']
+decades = ['1940','1950','1960','1970','1980','1990','2000','2010']
 
 # ['acousticness','danceability','energy','instrumentalness','liveness','loudness','speechiness','valence','tempo']
 features = ['acousticness','danceability','energy','instrumentalness','loudness','speechiness','valence','tempo']
 
+# load wikipedia csvs to dataframes
 def open_raw_csvs():
     decade_dfs = []
     for decade in decades:
-        df = pd.read_csv('decadeBillboards/{}s.csv'.format(decade),header=0,names=['date','artist','song','label','weeks','annotation'],encoding='Latin-1')
+        if decade == '1970':
+            df = pd.read_csv('decadeBillboards/{}s.csv'.format(decade),names=['date','artist','song','weeks','annotation'],encoding='Latin-1')
+        elif decade[0] == '2': # 2000 and 2010 are special
+            df = pd.read_csv('decadeBillboards/{}s.csv'.format(decade),names=['date','artist','song','weeks'],encoding='Latin-1')
+        else:
+            df = pd.read_csv('decadeBillboards/{}s.csv'.format(decade),names=['date','artist','song','label','weeks','annotation'],encoding='Latin-1')
         decade_dfs.append(df)
 
     return decade_dfs
 
+# load csvs that I cleaned into dataframes
 def open_clean_csvs():
     decade_dfs = []
     for decade in decades:
-        df = pd.read_csv('cleanedDecades/{}s_clean.csv'.format(decade),header=0,encoding='Latin-1',na_values=None)
+        df = pd.read_csv('cleanedDecades/{}s_clean.csv'.format(decade),header=0,encoding='Latin-1')
         decade_dfs.append(df)
 
     return decade_dfs
 
+'''
+take data from wikipedia and:
+    take the year from the "first reached #1" date
+    remove the null rows (in wikipedia table these are year headers)
+    reset the index so no missing numbers
+    remove unwanted characters
+    grab the columns we want
+'''
 def clean_dfs(dfs):
     ret = []
     for df in dfs:
         df['top_year'] = pd.to_datetime(df['date']).dt.year
         df = df[df[['top_year','artist','song']].notnull().all(1)]
         df = df.reset_index(drop=True)
+        df['song'] = df['song'].apply(lambda name: name.split('[')[0].strip()) # remove [....]
+
         df['song'] = df['song'].apply(lambda name: re.sub(r'[\?"]','',name)) # remove quotes and unknown characters
         ret.append(df[['top_year','artist','song']])
     return ret
 
+# initialize the json object as a dict with the keys we need
 def init_json():
-    json = {}
-    json['features'] = {}
+    data = {}
+    data['features'] = {}
     for feat in features:
-        json['features'][feat] = {}
-    json['decades'] = {}
+        data['features'][feat] = {}
+    data['decades'] = {}
+    for d in decades:
+        data['decades']['${}'.format(d)] = {}
+        data['decades']['${}'.format(d)]['features'] = {}
 
-    return json
+    return data
 
+# search for the song with the Spotify API and add the data we get (song id, url, etc)
 def append_api_values(row):
     result = sp.search(row['song'],row['artist'])
     for key, value in result.items():
         row[key] = value
     return row
 
+# take the decade data frame and song audio features and mush them together :)
 def add_features(df,sfs):
     def add_features_to_row(row,feats):
         for f in features:
             if not feats:
-                row[f] = np.nan
+                row[f] = np.nan # features are NaN if the search failed
             else:
                 row[f] = feats[f]
         return row
         
     df = df.apply(lambda row: add_features_to_row(row,sfs[row.name]),axis=1)
     return df
+
+# change a row into an object-like dictionary with keys of col_names plus "features"
+def to_dict(row,col_names):
+    ret = {}
+    for c in col_names:
+        ret[c] = row[c]
+    ret['features'] = {}
+    for f in features:
+        ret['features'][f] = row[f]
+
+    return ret
 
 
 
@@ -74,20 +112,40 @@ if __name__ == '__main__':
         for i, df in enumerate(dfs):
             print('{}\'s'.format(decades[i]),'-----------------')
             with_api_data = df.apply(lambda row: append_api_values(row), axis=1) # not with audio features yet
-            #print(with_api_data)
             with_api_data.to_csv('cleanedDecades/{}s_clean.csv'.format(decades[i]), index=False, encoding='Latin-1')
-        # pd.tocsv
 
     elif run == 'to json': # take cleaned csv's, get feature data, save formatted data as a json
         dfs = open_clean_csvs()
+        data = init_json()
         
         for i, df in enumerate(dfs):
+            print('{}\'s'.format(decades[i]),'-----------------')
+            # get audio features and slap them on dataframe
             song_features = sp.get_features(df['id'],features)
             dfs[i] = add_features(df,song_features)
-            print(dfs[i])
+            # average them
+            mean_features = dfs[i][features].mean()
+            # add the data to the json
+            for f in features:
+                data['features'][f]['${}'.format(decades[i])] = mean_features[f]
+                data['decades']['${}'.format(decades[i])]['features'][f] = mean_features[f]
+  
+            data['decades']['${}'.format(decades[i])]['songs'] = dfs[i].apply(lambda row: to_dict(row,['top_year','artist','song','released','url']),axis=1).tolist()
+            
+            
+        pp.pprint(data)
+        with open('data.json','w') as file:
+            json.dump(data,file)
+
+
         
+        
+
 
             
         
+
+
+
 
 
--- dataPreprocessing/spotipyUtils.py
+++ dataPreprocessing/spotipyUtils.py
@@ -14,18 +14,25 @@ def search(track,artist):
     q = 'track:"{}" artist:"{}"'.format(track,artist)
     result = sp.search(q,limit=1)
 
-    if len(result['tracks']['items']) == 0: # filter out punctuation
+    if len(result['tracks']['items']) == 0: # search UK market instead
+        result = sp.search(q,limit=1,market='GB')
+    if len(result['tracks']['items']) == 0: # remove / ... from track
+        q = 'track:"{}" artist:"{}"'.format(track.split('/')[0].strip(),artist)
         result = sp.search(re.sub(r'[-\.,\'!\(\)]','',q),limit=1)
-    if len(result['tracks']['items']) == 0: # remove parenthesis
+    if len(result['tracks']['items']) == 0: # remove parenthesis from track
         q = 'track:"{}" artist:"{}"'.format(track.split('(')[0].strip(),artist) 
+        result = sp.search(q,limit=1)
+    if len(result['tracks']['items']) == 0: # remove & ... from artist
+        q = 'track:"{}" artist:"{}"'.format(track,artist.split('&')[0].strip()) 
         result = sp.search(q,limit=1)
     if len(result['tracks']['items']) == 0: # remove "and ..." from artist
         q = 'track:"{}" artist:"{}"'.format(track,artist.split('and')[0].strip()) 
         result = sp.search(q,limit=1)
-    if len(result['tracks']['items']) == 0: # search UK market instead
-        result = sp.search(q,limit=1,market='GB')
+    if len(result['tracks']['items']) == 0: # filter out punctuation
+        result = sp.search(re.sub(r'[-\.,\'!\(\)]','',q),limit=1)
+
     if len(result['tracks']['items']) == 0:
-        print('not found:',artist,'-',track)
+        print('not found:',track,'-',artist)
         return {
             'released': ' ',
             'url': ' ',
@@ -88,7 +95,8 @@ if __name__ == '__main__':
 
     #print(get_features(search('Hello','Clairo')['id'],['danceability','energy','loudness']))
     #print(get_features('fakeid',['danceability','energy','loudness']))
-    print(get_features([search('Hello','Clairo')['id'],'fakeid'],['danceability','energy','loudness']))
+    #print(get_features([search('Hello','Clairo')['id'],'fakeid'],['danceability','energy','loudness']))
     #print(search('Green Eyes (Aquellos Ojos Verdes)','Jimmy Dorsey'))
-    #print(search('Hello','Clairo'))
+    #print(search("Its Gonna Be Me","N sync"))
     #print(test_search("I Love You For Sentimental Reasons",'King Cole'))
+    pass
